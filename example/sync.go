f3438f8ac9f3c11bd06df715743a2e8a88279742
tinyzimmer <38474291+tinyzimmer@users.noreply.github.com>
2018-07-22 18:42:45 -0700

    comment sample script

--- example/sync.go
+++ example/sync.go
@@ -8,15 +8,28 @@ import (
 )
 
 func main() {
+
+	// Connect to a Parity Node. We take the default localhost:8545 and
+	// use debug to see all output
+
 	node, err := parity.GetParityNode("", true)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Create a client for the eth api
+
 	client := eth.NewClient(node)
+
+	// query the sync status of the node
+
 	resp, err := client.Syncing()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Log the hexadecimal encoded responses and decode them as well.
+
 	log.Printf("Encoded: %+v\n", resp)
 	log.Printf("Decoded: %+v\n", resp.DecodeAll())
 }
