4fd0784f355fad65ec2c29b7ad2515393c3e8f22
tinyzimmer <38474291+tinyzimmer@users.noreply.github.com>
2018-07-23 06:21:23 -0700

    add example command

--- example/sync.go
+++ example/sync.go
@@ -8,6 +8,12 @@ import (
 	"github.com/tinyzimmer/parity-go/eth"
 )
 
+// Example command for starting Parity
+//docker run -ti -p 8180:8180 -p 8545:8545 -p 8546:8546 -p 30303:30303 -p 30303:30303/udp \
+//    --rm parity/parity --no-ancient-blocks --no-serve-light --max-peers 250 \
+//    --snapshot-peers 50 --min-peers 50 --mode active --tracing off --pruning fast \
+//    --db-compaction ssd --cache-size 4096 --chain kovan --jsonrpc-interface all
+
 func main() {
 
 	// Connect to a Parity Node. We take the default localhost:8545 and
